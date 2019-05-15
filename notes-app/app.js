f5406f2c4fb55499815936b61383302e0530900e
Misael Burboa <mburboa@pipingrock.com>
2019-05-15 09:41:39 -0700

    installing nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,10 @@
-const validator = require('validator');
+//const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
 
-console.log(validator.isEmail('cmburboa@gmail.com'));
-console.log(validator.isURL('http:///mead.io'));
+/* console.log(validator.isEmail('cmburboa@gmail.com'));
+console.log(validator.isURL('http:///mead.io')); */
+
+console.log(chalk.blue.inverse.bold("Success!"));
