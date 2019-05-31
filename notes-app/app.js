c0d152f289622640a650a2632c5cea3a20cf446d
Misael Burboa <mburboa@pipingrock.com>
2019-05-31 10:34:07 -0700

    Argument Passing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,59 @@
 //const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-/* console.log(getNotes());
+//customize yargs version
+yargs.version('1.1.0');
 
-console.log(validator.isEmail('cmburboa@gmail.com'));
-console.log(validator.isURL('http:///mead.io'));
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
+    }
+});
 
-console.log(chalk.blue.inverse.bold("Success!")); */
+// Create Remove Command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note!',
+    handler: function() {
+        console.log("Removing a note!");
+    }
+});
 
-const command = process.argv[2];
+// List Remove Command
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes!',
+    handler: function() {
+        console.log("Listing notes!");
+    }
+});
 
-console.log(process.argv);
+// Read Command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note!',
+    handler: function() {
+        console.log("Reading a note!");
+    }
+});
 
-/* switch (command) {
-    case "add": console.log("Adding note!"); break;
-    case "remove": console.log("Removing note!"); break;
-} */
+yargs.parse();
+// console.log(yargs.argv);
