5c232b8c32f507eef8b907ac55a4eaefd02fec81
Misael Burboa <mburboa@pipingrock.com>
2019-05-31 09:21:13 -0700

    get arguments from console

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,9 +2,18 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-console.log(getNotes());
+/* console.log(getNotes());
 
-/* console.log(validator.isEmail('cmburboa@gmail.com'));
-console.log(validator.isURL('http:///mead.io')); */
+console.log(validator.isEmail('cmburboa@gmail.com'));
+console.log(validator.isURL('http:///mead.io'));
 
-console.log(chalk.blue.inverse.bold("Success!"));
+console.log(chalk.blue.inverse.bold("Success!")); */
+
+const command = process.argv[2];
+
+console.log(process.argv);
+
+/* switch (command) {
+    case "add": console.log("Adding note!"); break;
+    case "remove": console.log("Removing note!"); break;
+} */
