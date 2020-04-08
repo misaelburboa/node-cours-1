946b8576e90ade0fdb8952586a25d858a14ec0db
Misael Burboa <cmburboa@gmail.com>
2020-04-08 08:26:24 -0400

    implemented patch, delete and organize files in the route folder

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const Task = require('../models/task');
+
+const router = new express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        res.send(await Task.find({}));
+    } catch (e) {
+        res.status(400).send({error: e});
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(400).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500);
+    }
+});
+
+router.post('/', async (req, res) => {
+    const task = new Task(req.body);
+
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (e) {
+        res.status(400).send();
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+
+        if (!task) {
+            res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(e);
+    }
+});
+
+module.exports = router;
